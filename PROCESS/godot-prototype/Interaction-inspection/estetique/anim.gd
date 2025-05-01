bbf7ef45d822a25f9920c5e28a3557bf17a21ba6
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-01 22:00:04 +0200

    integration level design grenier

--- PROCESS/godot-prototype/Interaction-inspection/estetique/anim.gd
+++ PROCESS/godot-prototype/Interaction-inspection/estetique/anim.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 func _ready():
 	animation_player.play("idle")  # Assure-toi que le nom correspond à celui dans l'inspecteur
