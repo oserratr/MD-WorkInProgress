bcbf0ea17c8c769455352e39e5df6e029cb2e6c1
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-02 00:21:30 +0200

    dupliquer scene pour test vue

--- PROCESS/godot-prototype/Interaction-inspection/estetique/anim.gd
+++ PROCESS/godot-prototype/Interaction-inspection/estetique/anim.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
-@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+#@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+@export var animation_player : AnimationPlayer
 
 func _ready():
 	animation_player.play("idle")  # Assure-toi que le nom correspond à celui dans l'inspecteur
