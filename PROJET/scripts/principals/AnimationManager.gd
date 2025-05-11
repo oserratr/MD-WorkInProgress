032fe2ae61c50dc08602303e92598adb2fa13b6c
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-11 18:25:44 +0200

    Loop and play animation via animation manager on mother

---
+++ PROJET/scripts/principals/AnimationManager.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+# Standing Idle Reading animation Mother
+@export var animation_player_mere: AnimationPlayer
+@export var animation_name_mere: String = "StandingIdleReading"
+
+func _ready():
+	# Standing Idle Reading animation Mother
+		animation_player_mere.play(animation_name_mere)
+	
