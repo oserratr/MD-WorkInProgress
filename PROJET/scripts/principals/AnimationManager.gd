cacf02fdf4077c2b6184ec763fbdc1b5d7829d51
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-12 23:26:14 +0200

    Integration model personnage jouable

--- PROJET/scripts/principals/AnimationManager.gd
+++ PROJET/scripts/principals/AnimationManager.gd
@@ -3,8 +3,11 @@ extends Node3D
 # Standing Idle Reading animation Mother
 @export var animation_player_mere: AnimationPlayer
 @export var animation_name_mere: String = "StandingIdleReading"
-
+@export var animation_player_fille: AnimationPlayer
+@export var animation_name_fille_walking: String = "walking"
+@export var animation_name_fille_idle: String = "idle"
 func _ready():
 	# Standing Idle Reading animation Mother
 		animation_player_mere.play(animation_name_mere)
+		animation_player_fille.play(animation_name_fille_idle)
 	
