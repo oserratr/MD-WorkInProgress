1da425c8cdca005518a390530bc12c24acd99d65
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-26 00:37:02 +0200

    update

--- PROJET/scripts/principals/AnimationManagerCutscene.gd
+++ PROJET/scripts/principals/AnimationManagerCutscene.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var animation_player_fille: AnimationPlayer
+@export var animation_player_fille_assoir: AnimationPlayer
 @export var animation_name_fille_talking: String = "talking"
+@export var animation_name_fille_assoir: String = "assoir"
 func _ready():
 	# Standing Idle Reading animation Mother
 		animation_player_fille.play(animation_name_fille_talking)
+		animation_player_fille_assoir.play(animation_name_fille_assoir)
