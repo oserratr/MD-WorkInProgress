1d4c28fbc6133c75ec4eb5b6f7354b0706f18b10
oserratr <85286206+oserratr@users.noreply.github.com>
2025-05-28 21:23:09 +0200

    Ui porte

--- PROJET/scripts/principals/InteractionCarton.gd
+++ PROJET/scripts/principals/InteractionCarton.gd
@@ -1,23 +1,25 @@
 extends Node3D
 
-var carton_pris:= false
-var player_enter_objet:= false
-@export var ui_interaction_prendre = Control
-@export var carton_pickable: Node3D
-@export var dialogue = Node3D
+var carton_pris := false
+var player_enter_objet := false
 var player_enter_salon := false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@export var ui_interaction_prendre: Control
+@export var carton_pickable: Node3D
+@export var dialogue: Node3D
+
 func _process(delta):
-	
 	var interaction_mother = dialogue.get_bool_interaction()
-	
-	#affiche l'ui
-	if player_enter_objet and not carton_pris and not interaction_mother :
+
+	# Affichage UI si :
+	# - le joueur est dans la zone du carton ET peut le prendre
+	# - OU dans la zone de la porte ET peut changer de scène
+	if (player_enter_objet and not carton_pris and not interaction_mother) \
+		or (player_enter_salon and not interaction_mother):
 		ui_interaction_prendre.visible = true
 	else:
 		ui_interaction_prendre.visible = false
-		
+
 	_interaction_carton()
 	_switch_salon()
 
@@ -28,8 +30,14 @@ func _interaction_carton():
 			print("prendre carton")
 			carton_pickable.queue_free()
 			carton_pris = true
-			
 
+func _switch_salon():
+	var interaction_mother = dialogue.get_bool_interaction()
+	if player_enter_salon and Input.is_action_just_pressed("e"):
+		if not interaction_mother:
+			get_tree().change_scene_to_file("res://scenes/niveaux/salon.tscn")
+
+# Zones : carton
 func _on_carton_2_body_entered(body):
 	if body.is_in_group("player"):
 		player_enter_objet = true
@@ -38,21 +46,14 @@ func _on_carton_2_body_exited(body):
 	if body.is_in_group("player"):
 		player_enter_objet = false
 
-func get_bool_carton_pris() -> bool:
-	return carton_pris
-
-func _switch_salon():
-	var interaction_mother = dialogue.get_bool_interaction()
-	
-	if player_enter_salon and Input.is_action_just_pressed("e"): 
-		if not interaction_mother :
-			get_tree().change_scene_to_file("res://scenes/niveaux/salon.tscn")
-		
+# Zones : porte vers salon
 func _on_salon_body_entered(body):
 	if body.is_in_group("player"):
 		player_enter_salon = true
 
-
 func _on_salon_body_exited(body):
 	if body.is_in_group("player"):
 		player_enter_salon = false
+
+func get_bool_carton_pris() -> bool:
+	return carton_pris
