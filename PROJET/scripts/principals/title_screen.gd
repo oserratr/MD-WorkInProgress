179a44ea302a6eca355d9ce5b71b0f2cc9836e6b
oserratr <85286206+oserratr@users.noreply.github.com>
2025-06-08 18:44:05 +0200

    Update title screen

--- PROJET/scripts/principals/title_screen.gd
+++ PROJET/scripts/principals/title_screen.gd
@@ -8,6 +8,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("PlayGame") :
 		$CanvasLayer/AnimationPlayer.play("disolve")
 		await $CanvasLayer/AnimationPlayer.animation_finished
-		get_tree().change_scene_to_file("res://scenes/Cutscene/mainintrocutscene.tscn")
+		get_tree().change_scene_to_file("res://s cenes/Cutscene/mainintrocutscene.tscn")
 		$CanvasLayer/AnimationPlayer.play_backwards("disolve")
 	
