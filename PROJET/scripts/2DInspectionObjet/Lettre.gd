3e9bcd05c6c24a4cac0903c03c052b266a1b04a0
oserratr <85286206+oserratr@users.noreply.github.com>
2025-06-09 12:37:06 +0200

    Trouver lettre UI innervoice chambre

--- PROJET/scripts/2DInspectionObjet/Lettre.gd
+++ PROJET/scripts/2DInspectionObjet/Lettre.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 @export var ui_node_path: Node2D  # L'UI à activer quand on clique
+var clicklettre = false
 
 func _ready():
 	input_pickable = true  # permet de détecter les clics
 	ui_node_path.visible = false
+	
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		ui_node_path.visible = true
+		clicklettre = true
 		print("click")
---
+++ PROJET/scripts/Chambre/InspectionObjetTrad/InspectionTiroir.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@export var ui_innervoice : Control
+@export var ui_echap : Control
+var lettre_trouve = false
+
+func _ready():
+	ui_innervoice.visible = false
+	ui_echap.visible = false
+
+func _process(delta):
+	var lettre_node = get_node("Lettre/Lettre")
+	
+	# Si on clique la lettre
+	if lettre_node.clicklettre and not lettre_trouve:
+		lettre_trouve = true
+		_innervoice()
+	
+	# Si l'UI d'échappement est visible et que le joueur appuie sur "ui_cancel"
+	if ui_echap.visible and Input.is_action_just_pressed("ui_cancel"):
+		# Remplace ici par le chemin vers la scène que tu veux charger
+		get_tree().change_scene_to_file("res://scenes/niveaux/chambre.tscn")
+
+
+func _innervoice():
+	ui_innervoice.visible = true
+	$InnerVoice/AnimationPlayer.play("Fade_up")
+	ui_echap.visible = false
+	$InnervoiceTimer.start(5.0)
+
+func _on_innervoice_timer_timeout():
+	ui_innervoice.visible = false
+	ui_echap.visible = true
