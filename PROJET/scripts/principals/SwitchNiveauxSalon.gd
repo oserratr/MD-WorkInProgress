3c39d11e29e9bd14923592444cba1f933f61727a
oserratr <85286206+oserratr@users.noreply.github.com>
2025-06-10 22:45:59 +0200

    switchsalon

--- PROJET/scripts/principals/SwitchNiveauxSalon.gd
+++ PROJET/scripts/principals/SwitchNiveauxSalon.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 var player_enter_chambre = false
+var player_enter_grenier = false
 @export var ui_interaction: Control
 
 func _ready():
@@ -27,6 +28,10 @@ func _process(delta):
 		ui_interaction.visible = true
 		if Input.is_action_just_pressed("e"):
 			_switch_chambre()
+	elif player_enter_grenier :
+		ui_interaction.visible = true
+		if Input.is_action_just_pressed("e"):
+			_switch_grenier()
 	else :
 		ui_interaction.visible = false
 	
@@ -37,6 +42,13 @@ func _switch_chambre():
 	await $"../CanvasLayer/AnimationPlayer".animation_finished
 	get_tree().change_scene_to_file("res://scenes/NiveauxLettre/chambrebis.tscn")
 	$"../CanvasLayer/AnimationPlayer".play_backwards("disolve")
+
+func _switch_grenier():
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+	$"../CanvasLayer/AnimationPlayer".play("disolve")
+	await $"../CanvasLayer/AnimationPlayer".animation_finished
+	get_tree().change_scene_to_file("res://scenes/niveaux/grenierApcarton.tscn")
+	$"../CanvasLayer/AnimationPlayer".play_backwards("disolve")
 	
 func _on_porte_chambre_body_entered(body):
 	if body.is_in_group("player"):
@@ -46,3 +58,13 @@ func _on_porte_chambre_body_entered(body):
 func _on_porte_chambre_body_exited(body):
 	if body.is_in_group("player"):
 		player_enter_chambre = false
+
+
+func _on_porte_grenier_body_entered(body):
+	if body.is_in_group("player"):
+		player_enter_grenier = true
+
+
+func _on_porte_grenier_body_exited(body):
+	if body.is_in_group("player"):
+		player_enter_grenier = false
