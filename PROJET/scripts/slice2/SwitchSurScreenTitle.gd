8a508434b26a62938eb3d7b75a038655cf2fd05d
oserratr <85286206+oserratr@users.noreply.github.com>
2026-01-28 12:14:47 +0100

    reparer choice hoover

--- PROJET/scripts/slice2/SwitchSurScreenTitle.gd
+++ PROJET/scripts/slice2/SwitchSurScreenTitle.gd
@@ -1,13 +1,23 @@
 extends Control
 
+var elapsed_time := 0.0
+var has_switched := false
+const TIMEOUT := 60.0
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	var my_music = preload("res://assets/Son/outro.mp3")
 	AudioManager.play_music(my_music)
 
 func _process(delta):
-	if Input.is_action_just_pressed("PlayGame"):
+	if has_switched:
+		return
+
+	elapsed_time += delta
+
+	if Input.is_action_just_pressed("PlayGame") or elapsed_time >= TIMEOUT:
 		_switch_ecran_screentitle()
 
 func _switch_ecran_screentitle():
+	has_switched = true
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
